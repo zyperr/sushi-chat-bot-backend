9e8d2945988ceed3363e5966dfc8d18151e2fcc1
Zyperr <maxi200vera@gmail.com>
2024-12-27 03:21:53 -0300

    se elimino la funcion createToken

--- src/security/hashing.js
+++ src/security/hashing.js
@@ -1,9 +1,8 @@
 import bscrypt from "bcrypt";
-import jwt from "jsonwebtoken"
 import {envVariables} from "../config/envVariables.js"
 
 
-const {SECRET_KEY,EXPIRATION_TIME,salt_Rounds} = envVariables
+const {salt_Rounds} = envVariables
 
 
 const hashPassword = async (password) => {
@@ -15,22 +14,9 @@ const verifyHash = async (password,hash) => {
     return result
 }
 
-const createToken = (id) => {
-    const token = jwt.sign(
-        {
-            id
-        },
-        SECRET_KEY
-        ,
-        {
-            expiresIn:EXPIRATION_TIME
-        }
-    )
-    return token
-}
+
 
 export {
     hashPassword,
-    verifyHash,
-    createToken
+    verifyHash
 }
