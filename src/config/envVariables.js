d1cd2806afbf66c34ed1224082ebb6147a38f268
Zyperr <maxi200vera@gmail.com>
2024-12-27 03:20:26 -0300

    se agregaron datos por defecto a algunas variables de entorno

--- src/config/envVariables.js
+++ src/config/envVariables.js
@@ -3,12 +3,12 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const envVariables = {
-    PORT: process.env.PORT,
-    PORT_DB: process.env.PORT_DB,
-    DB_NAME: process.env.DB_NAME,
+    PORT: process.env.PORT || 3000,
+    PORT_DB: process.env.PORT_DB || 27017,
+    DB_NAME: process.env.DB_NAME || "sushi",
     SECRET_KEY: process.env.SECRET_KEY,
-    EXPIRATION_TIME: process.env.EXPIRATION_TIME,
-    salt_Rounds: process.env.salt_Rounds
+    EXPIRATION_TIME: process.env.EXPIRATION_TIME || "1h",
+    salt_Rounds: parseInt(process.env.salt_Rounds) ||   10
 }
 
 export {envVariables}
