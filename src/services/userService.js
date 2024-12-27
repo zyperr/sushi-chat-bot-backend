10f0ae1403a4461e00c11c486af89cddd8db9684
Zyperr <maxi200vera@gmail.com>
2024-12-27 02:41:27 -0300

    se creo la logica para el registro y login de usuarios

---
+++ src/services/userService.js
@@ -0,0 +1,93 @@
+import { clientDb } from "../config/db.js";
+import {hashPassword,createToken,verifyHash} from "../security/hashing.js"
+import { ObjectId } from "mongodb";
+import {userModel,userLoginModel} from "../models/usuario.js";
+import { ZodError } from "zod";
+const getUser = async (id,res) =>  {
+    const user = await clientDb.collection("users").findOne({ _id: ObjectId.createFromHexString(id)});
+    if(!user){
+        res.status(404).json({message:`User with the id ${id} not found`});
+    }
+
+    res.status(200).json(user);
+}
+const createUser = async (user,res) =>  {
+    try{
+        //It validates user data with userModel
+        const validUser = userModel.parse(user);
+    
+        const userExists = await clientDb.collection("users").findOne({name: validUser.name});
+        if(userExists){
+            return res.status(400).send("User already exists")
+        }
+
+        let newUser = {
+            name: validUser.name,
+            email: validUser.email,
+            password: await hashPassword(validUser.password),
+            role: "user"
+        }
+            
+        const result = await clientDb.collection("users").insertOne(newUser);
+
+
+        res.status(201).json({
+            data: result,
+            message: "User created successfully"    
+        });
+        
+    }catch(err){
+        if (err instanceof ZodError) {
+            // Extraer y estructurar los errores
+            const errores = err.errors.map(err => ({
+              campo: err.path.join('.'),
+              mensaje: err.message,
+            }));
+        return res.status(400).json({ errores });
+    }
+}
+}
+
+
+const login = async (user, res) => {
+    try {
+        const validUser = userLoginModel.parse(user);
+        const userExists = await clientDb.collection("users").findOne({ name: validUser.name });
+
+        if (!userExists) {
+            return res.status(404).send("User not found");
+
+        }
+
+        const passwordMatch = await verifyHash(validUser.password, userExists.password);
+
+        if (!passwordMatch) {
+            return res.status(401).send("Invalid password");
+        }
+
+        const token = createToken(userExists._id);
+
+        if (!token) {
+            return res.status(500).json({ message: 'Token generation failed' });
+        }
+
+        return res.json({ message: 'Login successful', token });
+    } catch (err) {
+        if (err instanceof ZodError) {
+            const errores = err.errors.map(error => ({
+                campo: error.path.join('.'),
+                mensaje: error.message,
+            }));
+            return res.status(400).json({ errores });
+        }
+        
+        console.error('Unexpected error occurred:', err);
+        return res.status(500).json({ message: 'Internal server error' });
+    }
+}
+
+export {
+    createUser,
+    getUser,
+    login
+}
