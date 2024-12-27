0699bd71ad09a75df315b05c2f5068384f8a1046
Zyperr <maxi200vera@gmail.com>
2024-12-27 04:40:29 -0300

    se agrego la propiedad category a el modelo de productos

--- src/models/product.js
+++ src/models/product.js
@@ -5,5 +5,6 @@ export const productModel = z.object({
     name: z.string().min(3,{message:"Name must be at least 3 characters"}),
     price: z.number().min(0).positive({message:"Price must be a positive number"}),
     picture: z.string().min(3),
-    pieces: z.number().min(1).max(100)
+    pieces: z.number().min(1).max(100),
+    category: z.string().min(3).optional()
 })
