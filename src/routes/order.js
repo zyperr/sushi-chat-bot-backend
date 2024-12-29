1db05a72b6a14f38e3b25d3dbb3d456b4dfdc2c7
Zyperr <maxi200vera@gmail.com>
2024-12-28 21:21:04 -0300

    se creo variable  endpoint para los metodos

--- src/routes/order.js
+++ src/routes/order.js
@@ -3,8 +3,8 @@ import { createOrder } from "../services/orderService.js";
 
 
 const router = Router();
-
-router.post("/v1/orders",async (req,res) => {
+const endpoint = "/v1/orders"
+router.post(endpoint,async (req,res) => {
     const order = req.body;
 
     return createOrder(order,res);
