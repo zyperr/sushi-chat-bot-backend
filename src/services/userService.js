d8ec89f8b7cb46abb8618c6e63eec36c5e5df9c0
Zyperr <maxi200vera@gmail.com>
2024-12-28 21:27:19 -0300

    se creo getUser y se modifico getMe para que este no envie la contrañse al cliente se implemento la actualizacion de los pedidos del usuario ,obtener dichos pedidos y eliminarlos

--- src/services/userService.js
+++ src/services/userService.js
@@ -2,10 +2,9 @@ import { clientDb } from "../config/db.js";
 import {hashPassword,verifyHash} from "../security/hashing.js"
 import { createToken } from "../security/token.js";
 import { ObjectId } from "mongodb";
-import {userModel,userLoginModel} from "../models/usuario.js";
+import {userModel,userLoginModel} from "../models/users.js";
 import { ZodError } from "zod";
-
-
+import {filterSchema} from "../schema/filtro.js"
 
 const createUser = async (user,res) =>  {
     try{
@@ -21,10 +20,8 @@ const createUser = async (user,res) =>  {
         }
 
         let newUser = {
-            name: validUser.name,
-            email: validUser.email,
-            password: await hashPassword(validUser.password),
-            role: "user"
+            ...validUser,
+            password: await hashPassword(validUser.password)
         }
             
         const result = await clientDb.collection("users").insertOne(newUser);
@@ -32,7 +29,7 @@ const createUser = async (user,res) =>  {
         if(!result){
             return res.status(500).send("Error creating the user")
         }
-
+        console.log(newUser)
         res.status(201).json({
             data: result,
             message: "User created successfully"    
@@ -94,11 +91,84 @@ const getMe = async (id,res) => {
     if(!user){
         res.status(404).json({message:`User with the id ${id} not found`});
     }
-
-    res.status(200).json(user);
+   const {name,email,order,role,_id} = user
+    const parsedUser ={
+        _id,
+        name,
+        email,
+        order,
+        role
+    }
+    res.status(200).json(parsedUser);
 }
+const getUser = async (id) => {
+    const user = await clientDb.collection("users").findOne({ _id: ObjectId.createFromHexString(id)});
+    return {
+        name:user.name,
+        email:user.email,
+        role:user.role
+    }
+}
+const updateUserOrder = async (id,orderId,res) => {
+    await clientDb.collection("users").updateOne({ _id: ObjectId.createFromHexString(id) }, { $push: { order: orderId } });
+}
+
+const getOrderUser = async (id,res,querys) => {
+    try {
+        const user = await clientDb.collection("users").findOne({ _id: ObjectId.createFromHexString(id)});
+        if(!user){
+            res.status(404).json({message:`User with the id ${id} not found`});
+            return;
+        }
+
+        const f = filterSchema.parse(querys);
+
+        const {state,from,to,limit} = f;
+        const filterDict = {};
+
+
+        if(state){filterDict.state = state};
+        if(from || to){
+            filterDict.date = {};
+            if(from) filterDict.date.$gte = from;
+            if(to) filterDict.date.$lte = to;
+        }
+       
+
+        const limitResults = limit ? parseInt(limit) : 10;
+        const orders = await clientDb.collection("orders").find(filterDict).limit(limitResults).toArray();
+        
+        if(orders.length === 0){
+            res.status(200).json({message:"User has no order nothing yet",orders:[]});
+            return;
+        }
+
+        res.status(200).json({orders});
+    } catch (err) {
+        if (err instanceof ZodError) {
+            const errors = err.errors.map(error => ({
+                campo: error.path.join('.'),
+                mensaje: error.message,
+            }));
+            return res.status(400).json({ errors });
+        }
+    }
+}
+
+const deleteOrderFromUser = async (id,orderId,res) => {
+    if(!orderId){
+        res.status(400).json({message:"Missing required fields (orderId)"});
+    }
+    await clientDb.collection("users").updateOne({ _id: ObjectId.createFromHexString(id) }, { $pull: { order: ObjectId.createFromHexString(orderId) } });
+    res.status(200).json({message:"Order deleted successfully"})
+}
+
 export {
     createUser,
     login,
-    getMe
+    getMe,
+    updateUserOrder,
+    getOrderUser,
+    deleteOrderFromUser,
+    getUser
 }
