db09e2ddd176f8a9d180a83f3bd880a3b8ca031e
Zyperr <maxi200vera@gmail.com>
2024-12-28 21:22:46 -0300

    se creo endpoint para los meotodos crud

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,28 +1,40 @@
 import { Router } from "express";
-import { createUser,getMe,login } from "../services/userService.js";
+import { createUser,getMe,login,getOrderUser,deleteOrderFromUser} from "../services/userService.js";
 import { authenticateToken } from "../security/token.js";
 const router = Router();
 
-
-router.post("/v1/users/register",async(req,res) => {
+const endpoint = "/v1/users"
+router.post(`${endpoint}/register`,async(req,res) => {
     const user = req.body;
     return createUser(user,res);
 })
 
-router.post("/v1/users/login",async(req,res) => {
+router.post(`${endpoint}/login`,async(req,res) => {
     const user = req.body;
     console.log("loging")
     return login(user,res);
 })
 
-router.post("/v1/users/logout",async(req,res) => {
+router.post(`${endpoint}/logout`,async(req,res) => {
     return res.status(200).json({message:"Logout successful"});
 })
 
 
-router.get("/v1/users/me",authenticateToken , (req,res) => {
+router.get(`${endpoint}/me`,authenticateToken , (req,res) => {
     const id = req.user.id
     return getMe(id,res)
+})
+router.get(`${endpoint}/:id/order`,authenticateToken , (req,res) => {
+    const id = req.params.id
+    const querys = req.query
+    console.log(querys)
+    return getOrderUser(id,res,querys)
+})
+
+router.delete(`${endpoint}/me/removeOrder/:orderId`,authenticateToken , (req,res) => {
+    const id = req.user.id
+    const orderId = req.params.orderId
+    return deleteOrderFromUser(id,orderId,res)
 })
 
 export default router
