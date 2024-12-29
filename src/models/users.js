63fe5d73f86af9acb3dc2c1ba9bb45bda2266713
Zyperr <maxi200vera@gmail.com>
2024-12-28 21:20:12 -0300

    se creo enum para los roles y se agrego al modelo de usuario

--- src/models/users.js
+++ src/models/users.js
@@ -1,8 +1,14 @@
 import zod from "zod";
 
 
+const UserRole = Object.freeze({
+    USER: 'user',
+    ADMIN: 'admin'
+  });
+  
+const ROLE = zod.enum([UserRole.USER, UserRole.ADMIN]);
 
-export const userModel = zod.object({
+const userModel = zod.object({
     name: zod.string().min(3,{message:"Name must be at least 3 characters"}),
     //Checks to see if the text entered is a valid email address
     email: zod.string().email().regex(/^[\w-\\.]+@([\w-]+\.)+[\w-]{2,4}$/,{message:"Invalid email"}),
@@ -12,12 +18,21 @@ export const userModel = zod.object({
         - Can contain special characters
     */ 
     password: zod.string().min(8).regex(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/,{message:"Invalid password must contain at least 1 uppercase letter, 1 lowercase letter, and 1 number and at least 8 characters and also special characters"}),
-    role:zod.string().optional().default("user"),
+
+    role:ROLE.default("user"),
+
     order:zod.array(zod.string()).optional().default([])
 })
 
 
-export const userLoginModel = zod.object({
+const userLoginModel = zod.object({
     name: zod.string().min(3,{message:"Name must be at least 3 characters"}),
     password: zod.string().min(8).regex(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/,{message:"Invalid password must contain at least 1 uppercase letter, 1 lowercase letter, and 1 number and at least 8 characters and also special characters"})    
 })
+
+export {
+    userModel,
+    userLoginModel,
+    ROLE,
+    UserRole
+}
