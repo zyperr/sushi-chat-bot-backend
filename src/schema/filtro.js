8feab093a4d4949ea0e8c36ea03e86f5463a511b
Zyperr <maxi200vera@gmail.com>
2024-12-29 00:52:31 -0300

    se agrego funcion para manejar querys basicos

--- src/schema/filtro.js
+++ src/schema/filtro.js
@@ -2,9 +2,39 @@ import z from "zod";
 
 
 
-export const filterSchema = z.object({
-    state: z.enum(["pendiente", "entregado", "cancelado"]).optional().default("pendiente"),
-    from : z.string().optional().default(new Date().toISOString().slice(0, 10)),
-    to : z.string().optional().default(new Date().toISOString().slice(0, 10)),
-    limit: z.string().regex(/^\d+$/, 'Limit must be a number').default("1").optional(),
+const filterSchema = z.object({
+    state: z.enum(["pendiente", "entregado", "cancelado"]).optional(),
+    from : z.string().optional().default(),
+    to : z.string().optional().default(),
+    category: z.string().optional(),
 })
+
+
+const handleQuerys = (querys) => {
+    try {
+        let f = filterSchema.parse(querys);
+        const { state, from, to, category, price } = f;
+
+        const filterDict = {};
+
+        if (state) filterDict.state = state;
+        if (from || to) {
+            filterDict.date = {};
+            if (from) filterDict.date.$gte = from;
+            if (to) filterDict.date.$lte = to;
+        }
+        if (price) filterDict.price = parseInt(price, 10);
+        if (category) filterDict.category = category;
+
+        return { filterDict };
+    } catch (err) {
+        console.error('Error parsing querys:', err);
+        return { filterDict: {} };
+    }
+}
+
+
+
+export {
+    handleQuerys
+}
