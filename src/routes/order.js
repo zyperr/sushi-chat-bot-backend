f6360654fb92dc5516e99a6bd4afde6832bb350e
Zyperr <maxi200vera@gmail.com>
2024-12-31 22:57:29 -0300

    se agrego try catch para un mejor manejo de erorres

--- src/routes/order.js
+++ src/routes/order.js
@@ -5,9 +5,21 @@ import { createOrder } from "../services/orderService.js";
 const router = Router();
 const endpoint = "/v1/orders"
 router.post(endpoint,async (req,res) => {
-    const order = req.body;
-
-    return createOrder(order,res);
+    try{
+        const order = req.body;
+        if(!order){
+            return res.status(400).json({message:"Missing required fields"});
+        }
+        const response = await  createOrder(order);
+        if(!response){
+            return res.status(400).json({message:"Something went wrong"});
+        }
+        return res.json(response);
+    }catch(err){
+        console.log('Error', err);
+        return res.status(500).json({ message: 'Internal server error',error:err.message });
+    }
+    
 })
 
 
