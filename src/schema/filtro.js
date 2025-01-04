f9bf76fefe2230829c1cd728f90cc76c0769fbe4
Zyperr <maxi200vera@gmail.com>
2025-01-04 01:11:32 -0300

    se eliminaron filtros que no van a ser usados

--- src/schema/filtro.js
+++ src/schema/filtro.js
@@ -4,28 +4,23 @@ import z from "zod";
 
 const filterSchema = z.object({
     state: z.enum(["pendiente", "entregado", "cancelado"]).optional(),
-    from : z.string().optional().default(),
-    to : z.string().optional().default(),
     category: z.string().optional(),
+    total: z.string().optional(),
+    price : z.string().optional()
 })
 
 
 const handleQuerys = (querys) => {
     try {
         let f = filterSchema.parse(querys);
-        const { state, from, to, category, price } = f;
+        const { state, category, price,total } = f;
 
         const filterDict = {};
 
         if (state) filterDict.state = state;
-        if (from || to) {
-            filterDict.date = {};
-            if (from) filterDict.date.$gte = from;
-            if (to) filterDict.date.$lte = to;
-        }
+        if (total) filterDict.total = parseInt(total);
         if (price) filterDict.price = parseInt(price, 10);
         if (category) filterDict.category = category;
-
         return { filterDict };
     } catch (err) {
         console.error('Error parsing querys:', err);
