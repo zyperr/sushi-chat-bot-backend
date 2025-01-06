fe6e1d97c5cd146386a3ac626104b151868082d5
Zyperr <maxi200vera@gmail.com>
2025-01-06 17:14:38 -0300

    se agrego frontend url

--- src/config/envVariables.js
+++ src/config/envVariables.js
@@ -8,7 +8,8 @@ const envVariables = {
     DB_NAME: process.env.DB_NAME || "sushi",
     SECRET_KEY: process.env.SECRET_KEY,
     EXPIRATION_TIME: process.env.EXPIRATION_TIME || "1h",
-    salt_Rounds: parseInt(process.env.salt_Rounds) ||   10
+    salt_Rounds: parseInt(process.env.salt_Rounds) ||   10,
+    FRONTEND_URL: process.env.FRONTEND_URL || "http://localhost:5173",
 }
 
 export {envVariables}
